fd4766933ca9bf7db176cde1bf929fe4eb693f4b
w205 <w205@ip-172-31-20-147.ec2.internal>
2017-10-31 03:55:40 +0000

    adding hive_base_ddl.sql

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -11,7 +11,7 @@ mkdir ~/staging/exercise_1
 cd ~/staging/exercise_1
 
 #get file from data.medicare.gov
-MY_URL="https://data.medicare.gov/views/bg9k-emty/files/4a66c672-a92a-4ced-82a2-033c28581a90?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip" 
+MY_URL="https://data.medicare.gov/views/bg9k-emty/files/4a66c672-a92a-4ced-82a2-033c28581a90?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip"
 wget "$MY_URL" -O medicare_data.zip
 
 # unzip the medicare data
@@ -19,9 +19,9 @@ unzip medicare_data.zip
 
 # remove the first line of files and rename
 
-OLD_FILE_ARRAY=("Hospital General Information.csv" "Timely and Effective Case - Hospital.csv" "Complications and Deaths - Hospital.csv" "Measure Dates.csv" "hvbp_hcahps_11_10_2016.csv")
+OLD_FILE_ARRAY=("Hospital General Information.csv" "Timely and Effective Care - Hospital.csv" "Complications and Deaths - Hospital.csv" "Measure Dates.csv" "hvbp_hcahps_11_10_2016.csv")
 
-NEW_FILE_ARRAY=("hospitals.csv" "effective_care.csv" "readmissions.csv" "measures.csv" "surveys_responses.csv")
+NEW_FILE_ARRAY=("hospitals.csv" "effective_care.csv" "readmissions.csv" "measures.csv" "survey_responses.csv")
 
 for INDEX in ${!OLD_FILE_ARRAY[@]}; do
 	OLD_FILE="${OLD_FILE_ARRAY[INDEX]}"
@@ -33,11 +33,15 @@ done
 
 hdfs dfs -mkdir /user/w205/hospital_compare
 
-# copy the field to hdfs
+# create hdfs directory for each file and copy the file to hdfs
+
+SUB_DIR_ARR=("hospitals" "effective_care" "readmissions" "measures" "survey_responses")
 
 for INDEX in ${!NEW_FILE_ARRAY[@]}; do
 	NEW_FILE="${NEW_FILE_ARRAY[INDEX]}"
-	hdfs dfs -put "$NEW_FILE" /user/w205/hospital_compare
+	SUB_DIR="${SUB_DIR_ARR[INDEX]}"
+	hdfs dfs -mkdir /user/w205/hospital_compare/"$SUB_DIR"
+	hdfs dfs -put "$NEW_FILE" /user/w205/hospital_compare/"$SUB_DIR"
 done
 
 # change directory back to the original
@@ -47,4 +51,3 @@ cd $MY_CWD
 # clean exit
 
 exit
-
